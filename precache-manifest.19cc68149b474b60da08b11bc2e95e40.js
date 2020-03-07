6f9bdea0e636400c699d0416766d20164ccc8e27
hoangtulaptrinh <duong98kk@gmail.com>
2020-03-07 23:20:52 +0700

    Updates

---
+++ precache-manifest.19cc68149b474b60da08b11bc2e95e40.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbae7891239d96be3209eeb7ddb0ac38",
+    "url": "/wibu-never-die/index.html"
+  },
+  {
+    "revision": "573dbea2e181d7fe11db",
+    "url": "/wibu-never-die/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6c1e2b9d77e7a5474b45",
+    "url": "/wibu-never-die/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "573dbea2e181d7fe11db",
+    "url": "/wibu-never-die/static/js/2.7977687b.chunk.js"
+  },
+  {
+    "revision": "5a733b4f72d20e4afb54e249ef308592",
+    "url": "/wibu-never-die/static/js/2.7977687b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1e2b9d77e7a5474b45",
+    "url": "/wibu-never-die/static/js/main.48687a30.chunk.js"
+  },
+  {
+    "revision": "53f8ed6bfb619df2816d",
+    "url": "/wibu-never-die/static/js/runtime-main.c374a816.js"
+  },
+  {
+    "revision": "213676c00825691258aa34930b9ed1a4",
+    "url": "/wibu-never-die/static/media/Kabaneri_1366.213676c0.png"
+  },
+  {
+    "revision": "9195e0187134812a280e93fe92926b54",
+    "url": "/wibu-never-die/static/media/Kabaneri_1920.9195e018.png"
+  }
+]);
