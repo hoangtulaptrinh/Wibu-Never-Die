ab1afb9868740c2120628490c07eafeba50438dc
hoangtulaptrinh <duong98kk@gmail.com>
2020-05-24 21:00:31 +0700

    base admin

--- src/components/admin/Upload/Upload.js
+++ src/components/admin/Upload/Upload.js
@@ -1,26 +1,71 @@
 import React, { useState } from "react";
+import { Button } from "reactstrap";
+import axios from "axios";
+
+import UploadWrapper from "./Upload.style";
 
 const Upload = () => {
-  const [priview, setPriview] = useState([]);
+  const [priview, setPriview] = useState(null);
+  const [files, setFiles] = useState(null);
+
   const priviewImg = (event) => {
-    // const urlArray = event.target.files.map((file) =>
-    //   URL.createObjectURL(file)
-    // );
-    // setPriview(urlArray);
-    console.log(event.target.files);
+    const objectImg = event.target.files;
+    const files = Array.from(objectImg);
+    Promise.all(
+      files.map((file) => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.addEventListener("load", (ev) => {
+            resolve(ev.target.result);
+          });
+          reader.addEventListener("error", reject);
+          reader.readAsDataURL(file);
+        });
+      })
+    ).then(
+      (images) => {
+        /* Once all promises are resolved, update state with image URI array */
+        setPriview(images);
+        setFiles(objectImg);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  };
+
+  const handleSubmit = () => {
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append(`images[${i}]`, files[i]);
+    }
+    axios
+      .post("/log_in", formData)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   };
   return (
-    <div>
+    <UploadWrapper>
       <input
         type="file"
         accept="image/png, image/jpeg"
         multiple
         onChange={priviewImg}
       />
-      {priview.map((img, index) => (
-        <img src={img} alt="img" />
-      ))}
-    </div>
+      {!!priview && (
+        <>
+          <Button onClick={handleSubmit} color="success">
+            Upload Image
+          </Button>
+          {priview.map((img, index) => (
+            <img src={img} alt="img" key={index} />
+          ))}
+          <Button onClick={handleSubmit} color="success">
+            Upload Image
+          </Button>
+        </>
+      )}
+    </UploadWrapper>
   );
 };
 
---
+++ src/components/admin/Upload/Upload.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const UploadWrapper = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+img{
+  max-height:80vh;
+}
+}
+`;
+
+export default UploadWrapper;
